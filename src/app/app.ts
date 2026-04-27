b3af129dd32e4258f12bb2f540a9fe7baf54b232
aminhl <amin.hlel@esprit.tn>
2026-04-27 18:29:35 +0200

    init commit, setup tailwindcss & spartan-ng

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HlmAccordionImports} from '@spartan-ng/helm/accordion';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmAccordionImports],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('aminhlel');
+  protected readonly title = signal('Amin Hlel\'s Blog | Software engineering and Java');
 }
