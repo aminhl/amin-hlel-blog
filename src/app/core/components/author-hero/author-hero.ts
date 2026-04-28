a3a59965e17f4c933b8ed3ca287668081d7406dd
aminhl <amin.hlel@esprit.tn>
2026-04-28 19:39:47 +0200

    feat: complete the author-hero component

--- src/app/core/components/author-hero/author-hero.ts
+++ src/app/core/components/author-hero/author-hero.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import {AsyncPipe} from '@angular/common';
+import {HlmButton} from '@spartan-ng/helm/button';
+import {HlmIcon} from '@spartan-ng/helm/icon';
+import {NgIcon, provideIcons} from '@ng-icons/core';
+import {radixEnvelopeClosed} from '@ng-icons/radix-icons';
+import {
+  remixFacebookFill,
+  remixGithubLine,
+  remixLinkedinFill,
+  remixRssFill,
+  remixTwitterXFill
+} from '@ng-icons/remixicon';
 
 @Component({
   selector: 'app-author-hero',
-  imports: [],
+  imports: [
+    HlmButton,
+    HlmIcon,
+    NgIcon
+  ],
+  providers: [
+    provideIcons({radixEnvelopeClosed, remixGithubLine, remixTwitterXFill, remixFacebookFill, remixLinkedinFill, remixRssFill})
+  ],
   templateUrl: './author-hero.html',
   styleUrl: './author-hero.css',
 })
