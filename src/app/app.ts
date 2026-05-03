c1c3cf90151022a90fe7d595aa9097f79232e6f7
aminhl <amin.hlel@esprit.tn>
2026-05-03 14:44:23 +0200

    feat: create the blog-card component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Navbar} from './shared/components/navbar/navbar';
 import {AuthorHero} from './core/components/author-hero/author-hero';
+import {BlogCard} from './core/components/blog-card/blog-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, AuthorHero],
+  imports: [RouterOutlet, Navbar, AuthorHero, BlogCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
