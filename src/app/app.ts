75a712ce9752009a4ea1384dbacd4b0ebe743856
aminhl <amin.hlel@esprit.tn>
2026-07-04 22:05:20 +0200

    enhance app base components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Navbar} from './shared/components/navbar/navbar';
-import {AuthorHero} from './core/components/author-hero/author-hero';
-import {BlogCard} from './core/components/blog-card/blog-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, AuthorHero, BlogCard],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
